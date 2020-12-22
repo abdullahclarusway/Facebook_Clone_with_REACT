8869f83168a98461e3655de788ce5a7281a9d204
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2020-12-22 15:01:40 +0100

    Initialize Project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    // BEM naming convention
+    <div className="App">
+      <h1>Lets build the facebook clone</h1>
+    </div>
+  );
+}
+
+export default App;
