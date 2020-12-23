4fa14327c7b0efdfe9c11c1d319aedc222bc9a11
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2020-12-23 01:00:15 +0100

    Add style to header

--- src/Header.js
+++ src/Header.js
@@ -7,58 +7,59 @@ import SubscriptionsOutlinedIcon from "@material-ui/icons/SubscriptionsOutlined"
 import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlined";
 import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
 import { Avatar, IconButton } from "@material-ui/core";
-import AddIcon from '@material-ui/icons/Add';
-import ForumIcon from '@material-ui/icons/Forum';
-import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from "@material-ui/icons/Add";
+import ForumIcon from "@material-ui/icons/Forum";
+import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const Header = () => {
   return (
     <div className="header">
-      <div className="header__left"></div>
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZrEbOxezWT8pa0pUI-FfToi80Z00xjn5Wcw&usqp=CAU"
-        alt="Facebook-logo"
-      />
-      <div className="header__input">
-        <SearchIcon />
-        <input type="text" placeholder="Search Facebook"/>
+      <div className="header__left">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZrEbOxezWT8pa0pUI-FfToi80Z00xjn5Wcw&usqp=CAU"
+          alt="Facebook-logo"
+        />
+        <div className="header__input">
+          <SearchIcon />
+          <input type="text" placeholder="Search Facebook" />
+        </div>
       </div>
-
-      <div className="header__center"></div>
-      <div className="header__option header__option--active">
-        <HomeIcon fontSize="large" />
-      </div>
-      <div className="header__option">
-        <FlagIcon fontSize="large" />
-      </div>
-      <div className="header__option">
-        <SubscriptionsOutlinedIcon fontSize="large" />
-      </div>
-      <div className="header__option">
-        <StorefrontOutlinedIcon fontSize="large" />
-      </div>
-      <div className="header__option">
-        <SupervisedUserCircleIcon fontSize="large" />
+      <div className="header__center">
+        <div className="header__option header__option--active">
+          <HomeIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <FlagIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SubscriptionsOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <StorefrontOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SupervisedUserCircleIcon fontSize="large" />
+        </div>
       </div>
       <div className="header__right">
         <div className="header__info">
           <Avatar />
           <h4> Abdullah </h4>
         </div>
+        <IconButton>
+          <AddIcon />
+        </IconButton>
+        <IconButton>
+          <ForumIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsActiveIcon />
+        </IconButton>
+        <IconButton>
+          <ExpandMoreIcon />
+        </IconButton>
       </div>
-      <IconButton>
-          <AddIcon/>
-      </IconButton>
-      <IconButton>
-          <ForumIcon/>
-      </IconButton>
-      <IconButton>
-          <NotificationsActiveIcon/>
-      </IconButton>
-      <IconButton>
-          <ExpandMoreIcon/>
-      </IconButton>
     </div>
   );
 };
