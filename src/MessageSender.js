d960d5d11356e7ef60e9717cd7c2e1ccd68c06bd
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2020-12-24 20:02:08 +0100

    Add MessageSender and its style

---
+++ src/MessageSender.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "./MessageSender.css";
+import { Avatar } from "@material-ui/core";
+import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import VideocamIcon from "@material-ui/icons/Videocam";
+
+const MessageSender = () => {
+  const [input, setInput] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //db
+    setInput("");
+    setImageUrl("");
+
+  };
+  return (
+    <div className="messageSender">
+      <div className="messageSender__top">
+        <Avatar />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="messageSender__input"
+            type="text"
+            placeholder="What's your mind?"
+          />
+          <input
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+            type="text"
+            placeholder="Image URL (Optional)"
+          />
+          <button type="submit" onClick={handleSubmit}>
+            Hidden Submit
+          </button>
+        </form>
+      </div>
+      <div className="messageSender__bottom">
+        <div className="messageSender__option">
+          <VideocamIcon style={{ color: "red" }} />
+          <h3>Live Video</h3>
+        </div>
+        <div className="messageSender__option">
+          <PhotoLibraryIcon style={{ color: "green" }} />
+          <h3>Photo/Video</h3>
+        </div>
+        <div className="messageSender__option">
+          <InsertEmoticonIcon style={{ color: "orange" }} />
+          <h3>Feeling/Activity</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MessageSender;
