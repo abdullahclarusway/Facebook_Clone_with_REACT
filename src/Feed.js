15854868e1b3923e172990321f33666ee85d9823
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2020-12-24 22:14:16 +0100

    Add posts and their style

--- src/Feed.js
+++ src/Feed.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React from "react";
 import "./Feed.css";
-import MessageSender from './MessageSender';
-import StoryReel from './StoryReel';
+import MessageSender from "./MessageSender";
+import Post from "./Post";
+import StoryReel from "./StoryReel";
 
 const Feed = () => {
-    return (
-        <div className = "feed">
-            <StoryReel/>
-            <MessageSender/>
+  return (
+    <div className="feed">
+      <StoryReel />
+      <MessageSender />
+      <Post
+        profilePic="https://images.vexels.com/media/users/3/145908/preview2/52eabf633ca6414e60a7677b0b917d92-maennlicher-avatar-hersteller.jpg "
+        message="Wow. This works"
+        timestamp="This a timestamp"
+        username="Abdullah"
+        image="https://i.pinimg.com/originals/49/5b/a7/495ba7b612cdcd7512bab89570486364.jpg"
+      />
+      <Post
+        profilePic="https://images.vexels.com/media/users/3/145908/preview2/52eabf633ca6414e60a7677b0b917d92-maennlicher-avatar-hersteller.jpg "
+        message="Wow. This works"
+        timestamp="This a timestamp"
+        username="Abdullah"
+        image="https://i.pinimg.com/originals/49/5b/a7/495ba7b612cdcd7512bab89570486364.jpg"
+      />
+      <Post
+        profilePic="https://images.vexels.com/media/users/3/145908/preview2/52eabf633ca6414e60a7677b0b917d92-maennlicher-avatar-hersteller.jpg "
+        message="Wow. This works"
+        timestamp="This a timestamp"
+        username="Abdullah"
+        image="https://i.pinimg.com/originals/49/5b/a7/495ba7b612cdcd7512bab89570486364.jpg"
+      />
+    </div>
+  );
+};
 
-            
-            {/* Messagesender */}
-        </div>
-    )
-}
-
-export default Feed
+export default Feed;
