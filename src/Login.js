d8bb234d60b31895feab08d4686df7785bdc179f
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2020-12-25 01:37:56 +0100

    Connect to Firebase Auth

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,16 @@
 import React from "react";
 import "./Login.css";
 import { Button } from '@material-ui/core';
+import { auth, provider} from "./firebase";
 const Login = () => {
     const signIn = ()=>{
         //sign in...
+        auth.signInWithPopup(provider)
+        .then(result =>{
+            console.log(result.user)
+        }).catch((error)=>{
+            alert(error.message)
+        })
     }
   return (
     <div className="login">
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+import firebase from "firebase";
 const firebaseConfig= {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -6,4 +7,12 @@ const firebaseConfig= {
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_ID,
   };
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export {auth, provider};
+  export default db;
 
