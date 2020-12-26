edb27e43112c9b6a69cd28a195d984441f758ddf
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2020-12-26 01:17:21 +0100

    Connect to Firebase Cloud Firestore/Database

--- src/Feed.js
+++ src/Feed.js
@@ -1,37 +1,46 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./Feed.css";
 import MessageSender from "./MessageSender";
 import Post from "./Post";
-import { useStateValue } from "./StateProvider";
 import StoryReel from "./StoryReel";
+import db from "./firebase";
 
 const Feed = () => {
-    const [{ user }, dispatch] = useStateValue();
+    const [posts, setPosts] = useState([]);
+    
+
+    useEffect(() => {
+      db.collection("posts").onSnapshot((snapshot) =>
+        setPosts(snapshot.docs.map((doc)=>({id:doc.id , data :doc.data() }))) 
+      );
+     
+    }, []);
+    
     return (
     <div className="feed">
       <StoryReel />
       <MessageSender />
       <Post
-        profilePic={user.photoURL}
+        profilePic="https://i.imgur.com/I80W1Q0.png"
         message="Wow. This works"
         timestamp="This a timestamp"
-        username={user.displayName}
+        username="Abdullah"
+        image="https://i.pinimg.com/originals/49/5b/a7/495ba7b612cdcd7512bab89570486364.jpg"
+      />
+     <Post
+        profilePic="https://i.imgur.com/I80W1Q0.png"
+        message="Wow. This works"
+        timestamp="This a timestamp"
+        username="Abdullah"
         image="https://i.pinimg.com/originals/49/5b/a7/495ba7b612cdcd7512bab89570486364.jpg"
       />
       <Post
-        profilePic={user.photoURL}        
-        message="Wow. This works"
-        timestamp="This a timestamp"
-        username={user.displayName}
-        image="https://i.pinimg.com/originals/49/5b/a7/495ba7b612cdcd7512bab89570486364.jpg"
-      />
-      <Post
-        profilePic={user.photoURL}          
-        message="Wow. This works"
-        timestamp="This a timestamp"
-        username={user.displayName}
-        image="https://i.pinimg.com/originals/49/5b/a7/495ba7b612cdcd7512bab89570486364.jpg"
-      />
+      profilePic="https://i.imgur.com/I80W1Q0.png"
+      message="Wow. This works"
+      timestamp="This a timestamp"
+      username="Abdullah"
+      image="https://i.pinimg.com/originals/49/5b/a7/495ba7b612cdcd7512bab89570486364.jpg"
+    />
     </div>
   );
 };
